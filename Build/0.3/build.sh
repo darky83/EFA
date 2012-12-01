693b19f5ff35e9283678bfa7b837a6f90d104db2
darky83 <arno@troublenow.org>
2012-12-01 22:27:54 +0100

    EFA
    added license

--- Build/0.3/build.sh
+++ Build/0.3/build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-# +---------------------------------------------------+
+# +--------------------------------------------------------------------+
 # EFA build script v 0.3-20121111
-# 
-# Written by Arno Haverlach
-# Email arno@troublenow.org
+# +--------------------------------------------------------------------+
+# Copyright (C) 2012  http://www.efa-project.org
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# +--------------------------------------------------------------------+
 #
 # This creates the base for EFA.
-# +---------------------------------------------------+
-# Todo
-# - Create initial configure script
-#
 # +---------------------------------------------------+
 # STAGE 0 (Pre requirements for barebone install)
 # +---------------------------------------------------+
